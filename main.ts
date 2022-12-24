da92dce170af427714e42615e9bdef42dd3e2563
pmonber325 <121339862+pmonber325@users.noreply.github.com>
2022-12-24 11:42:37 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,42 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        . # # # .
+        . . . . .
+        . # . # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # . . . #
+        . # # # .
+        . . . . .
+        . # . # .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        `)
+    basic.pause(100)
+})
